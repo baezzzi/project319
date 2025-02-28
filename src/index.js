8894ee47e6260ce427cc6e21d98c3fd61d73823f
jiyeon <hannee233@dankook.ac.kr>
2025-02-28 17:27:30 +0900

    router 설정 변경

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './reportWebVitals';
-import { Route, Routes, Router } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from "./js/Home";
 import First from "./js/First";
@@ -12,7 +12,7 @@ import Game from "./js/Game";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/First" element={<First />} />
@@ -20,7 +20,7 @@ root.render(
       <Route path="/First/Player" element={<Player />} />
       <Route path="/First/Game" element={<Game />} />
     </Routes>
-  </Router>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

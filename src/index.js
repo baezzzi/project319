91798991b0d75ff5387f5d7f3896ab230731561e
jiyeon <hannee233@dankook.ac.kr>
2025-03-03 12:51:45 +0900

    basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Game from "./js/Game";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename='/'>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/First" element={<First />} />
